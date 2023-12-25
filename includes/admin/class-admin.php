34f6f33ad6b44133c03468b4ab8d85cd63cc9910
umdevelopera <umdevelopera@gmail.com>
2023-12-25 22:16:23 +0200

    - added "Background color" setting

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -26,11 +26,19 @@ if ( ! class_exists( 'um_ext\um_account_tabs\admin\Admin' ) ) {
 		 * Admin constructor.
 		 */
 		public function __construct() {
+			// Menu.
 			add_action( 'admin_menu', array( &$this, 'create_admin_submenu' ), 1001 );
+
+			// Columns.
+			add_filter( 'manage_edit-um_account_tabs_columns', array( $this, 'columns_th' ) );
+			add_action( 'manage_um_account_tabs_posts_custom_column', array( $this, 'columns_td' ), 10, 2 );
+
+			// Metaboxes.
 			add_action( 'load-post.php', array( &$this, 'init_metaboxes' ), 9 );
 			add_action( 'load-post-new.php', array( &$this, 'init_metaboxes' ), 9 );
-			add_filter( 'um_is_ultimatememeber_admin_screen', array( &$this, 'is_um_screen' ), 10, 1 );
 			add_filter( 'wp_insert_post_data', array( &$this, 'filter_post_data' ), 10, 4 );
+
+			add_filter( 'um_is_ultimatememeber_admin_screen', array( &$this, 'is_um_screen' ), 10, 1 );
 		}
 
 
@@ -63,6 +71,62 @@ if ( ! class_exists( 'um_ext\um_account_tabs\admin\Admin' ) ) {
 					um_account_tabs_version,
 					true
 				);
+			}
+		}
+
+
+		/**
+		 * Adds custom columns to the table "Account tabs".
+		 *
+		 * @param array $columns  An array of columns.
+		 *
+		 * @return array
+		 */
+		public function columns_th( $columns ) {
+			if ( isset( $columns['date'] ) ) {
+				unset( $columns['date'] );
+			}
+
+			$columns['position'] = __( 'Position', 'um-account-tabs' );
+			$columns['form']     = __( 'Embed form', 'um-account-tabs' );
+			$columns['roles']    = __( 'Roles restriction', 'um-account-tabs' );
+			$columns['date']     = __( 'Date', 'um-account-tabs' );
+
+			return $columns;
+		}
+
+
+		/**
+		 * Displays custom columns in the table "Account tabs"
+		 *
+		 * @param  string $column_name  A column slug.
+		 * @param  int    $id           Post ID.
+		 */
+		public function columns_td( $column_name, $id ) {
+			switch ( $column_name ) {
+
+				case 'position':
+					$position = get_post_meta( $id, '_position', true );
+					if ( $position ) {
+						echo absint( $position );
+					}
+					break;
+
+				case 'form':
+					$form_id = get_post_meta( $id, '_um_form', true );
+					if ( $form_id ) {
+						echo get_the_title( $id ) . ' (' . $id . ')';
+					}
+					break;
+
+				case 'roles':
+					$roles = get_post_meta( $id, '_can_have_this_tab_roles', true );
+					if ( $roles ) {
+						$all_roles = wp_roles()->role_names;
+						$roles     = array_intersect_key( $all_roles, array_flip( $roles ) );
+						echo implode( ', ', $roles );
+					}
+					break;
 			}
 		}
 
@@ -153,8 +217,8 @@ if ( ! class_exists( 'um_ext\um_account_tabs\admin\Admin' ) ) {
 			);
 
 			add_meta_box(
-				'um-admin-custom-account-tab/icon{' . um_account_tabs_path . '}',
-				__( 'Customize this tab', 'um-account-tabs' ),
+				'um-admin-custom-account-tab/appearance{' . um_account_tabs_path . '}',
+				__( 'Appearance', 'um-account-tabs' ),
 				array( UM()->metabox(), 'load_metabox_custom' ),
 				'um_account_tabs',
 				'side',
@@ -208,17 +272,14 @@ if ( ! class_exists( 'um_ext\um_account_tabs\admin\Admin' ) ) {
 			}
 			update_post_meta( $post_id, '_can_have_this_tab_roles', $roles );
 
-			$icon = '';
-			if ( isset( $input['_icon'] ) ) {
-				$icon = sanitize_key( $input['_icon'] );
-			}
+			$color = isset( $input['_color'] ) ? sanitize_hex_color( $input['_color'] ) : '';
+			update_post_meta( $post_id, '_color', $color );
+
+			$icon = isset( $input['_icon'] ) ? sanitize_key( $input['_icon'] ) : '';
 			update_post_meta( $post_id, '_icon', $icon );
 
-			$icon = '';
-			if ( isset( $input['_position'] ) ) {
-				$icon = sanitize_key( $input['_position'] );
-			}
-			update_post_meta( $post_id, '_position', $icon );
+			$position = isset( $input['_position'] ) ? absint( $input['_position'] ) : '';
+			update_post_meta( $post_id, '_position', $position );
 		}
 	}
 }
